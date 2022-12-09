bbe119e1fd88c724b5421940c7473c41e21414e1
samplejavaproject <63184958+samplejavaproject@users.noreply.github.com>
2022-12-09 15:44:05 +0530

    Update ThreadSample.java

--- threadpracticeprogram/src/threadpracticeprogram/ThreadSample.java
+++ threadpracticeprogram/src/threadpracticeprogram/ThreadSample.java
@@ -6,7 +6,7 @@ public class ThreadSample extends Thread
 	{ 
 		for(int i=0;i<10;i++)
 		{
-			System.out.println("first thread program hello");
+			System.out.println("first thread program hello  hello pytho pro");
 		}
 	
      }
